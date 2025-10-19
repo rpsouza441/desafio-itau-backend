2b7e9a6436ff7aac911a86f1a9c6f195b322700b
rpsouza441 <rpsouza@hotmail.co.uk>
2025-10-19 17:16:41 -0300

    chore(init): preparar projketo e configurar dependencias iniciais
    Configurando o spring boot e adicionando dependencias necessarias para validação e documentação.

---
+++ estatistica-transacao/src/main/java/br/dev/rodrigopinheiro/estatistica_transacao/EstatisticaTransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigopinheiro.estatistica_transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatisticaTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatisticaTransacaoApplication.class, args);
+	}
+
+}
---
+++ estatistica-transacao/src/test/java/br/dev/rodrigopinheiro/estatistica_transacao/EstatisticaTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigopinheiro.estatistica_transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstatisticaTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
